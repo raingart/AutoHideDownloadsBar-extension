d840be0ae525ed93d202dae068ccea531282f552
Artlant <xufo@ukr.net>
2017-01-03 20:12:35 +0200

    removed unnecessary operation [DownloadChanged]

--- js/bg.js
+++ js/bg.js
@@ -1,37 +1,39 @@
-// 'use strict';
+'use strict';
 
 chrome.downloads.onCreated.addListener(function(item) {
     DownloadCreated(item);
 });
 
 chrome.downloads.onChanged.addListener(function(item) {
-    DownloadChanged(item);
+    let sd = localStorage.showdownbar == 'true' ? true : false;
+    if (sd === true) {
+      DownloadChanged(item);
+    }
 });
 
 function DownloadCreated(item) {
-    var sdbar = localStorage.showdownbar == 'true' ? true : false;
-    chrome.downloads.setShelfEnabled(sdbar);
-    DownloadChanged(item);
+    let sd = localStorage.showdownbar == 'true' ? true : false;
+    chrome.downloads.setShelfEnabled( sd );
 }
 
 function DownloadChanged(item) {
-    //~ if ((item.state.current == 'complete') && item.endTime && !item.error) {
-    //~ chrome.downloads.setShelfEnabled(false);
-    //~ }
-
     chrome.downloads.search({}, function(items) {
-        var now_progress = false;
+        let now_progress = false;
 
         items.forEach(function(item) {
+            // console.log(item.state);
             if (item.state == 'in_progress') {
                 now_progress = true;
                 return false;
             }
-            //~ else if ((item.state == 'complete') && item.endTime && !item.error) {
-            //~ }
+            // else if ((item.state.current == 'complete') && item.endTime && !item.error)
+            //     now_progress = false;
+            // else
+            //   now_progress = true;
         });
-        if (now_progress === false) {
-            chrome.downloads.setShelfEnabled(false);
-        }
+        chrome.downloads.setShelfEnabled( now_progress );
+        // if (now_progress === false) {
+            // chrome.downloads.setShelfEnabled(false);
+        // }
     });
 }
