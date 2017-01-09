3ec6011517563a7da9ceefbe56ae2f9eca8a6df5
Artlant <xufo@ukr.net>
2017-01-09 23:39:10 +0200

    add icon counter

--- js/bg.js
+++ js/bg.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 chrome.downloads.onCreated.addListener(function(item) {
     DownloadCreated(item);
@@ -6,9 +6,9 @@ chrome.downloads.onCreated.addListener(function(item) {
 
 chrome.downloads.onChanged.addListener(function(item) {
     let sd = localStorage.showdownbar == 'true' ? true : false;
-    if (sd === true) {
+    // if (sd === true) {
       DownloadChanged(item);
-    }
+    // }
 });
 
 function DownloadCreated(item) {
@@ -19,12 +19,15 @@ function DownloadCreated(item) {
 function DownloadChanged(item) {
     chrome.downloads.search({}, function(items) {
         let now_progress = false;
+        var count = 0;
 
         items.forEach(function(item) {
             // console.log(item.state);
             if (item.state == 'in_progress') {
                 now_progress = true;
-                return false;
+                count = ++count;
+                console.log(count);
+                // return false;
             }
             // else if ((item.state.current == 'complete') && item.endTime && !item.error)
             //     now_progress = false;
@@ -35,5 +38,14 @@ function DownloadChanged(item) {
         // if (now_progress === false) {
             // chrome.downloads.setShelfEnabled(false);
         // }
+        flashBadge(count);
     });
+}
+
+function flashBadge( message ) {
+    // chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+    if (message === 0) {
+        message = "";
+    }
+    chrome.browserAction.setBadgeText({ text: message.toString() });
 }
