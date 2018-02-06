021f223910ade9c9a2269d6f91a710e65466a5ee
Artlant <xufo@ukr.net>
2018-02-06 21:38:10 +0200

    v2.1.2

--- release.sh
+++ release.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-# chmod a+x name.sh
+# chmod a+x release.sh
 
 FILENAME='chrome_bar'
 # TODAY=$(date)
 
 rm -v $FILENAME.zip
-# zip -r chrome_bar.zip _locales\* css\* html\* icons\* images\* js\* lib\* LICENSE manifest.json
-# zip -rf $FILENAME.zip \
 zip -r $FILENAME.zip \
                   _locales \
                   css/*/*.css \
